c2024237d3243512ec5a30a5718b6fbf682ffa71
Xiaochen Xu <xiaochex@gmail.com>
2021-06-07 16:48:56 -0400

    commit tree a2053645c454e02cbfe71ee374a64fafac53bd2c

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    @@all = []
 
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| song.genre == self}
+    end
+
+    def artists
+        songs.map(&:artist)
+    end
+end
